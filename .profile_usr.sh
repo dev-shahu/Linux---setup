24ba192e30faa0a3c891440342288c0659559978
Neeraj shah <86927608+dev-shahu@users.noreply.github.com>
2022-06-02 13:25:22 +0530

    Create .profile_usr.sh

---
+++ .profile_usr.sh
@@ -0,0 +1,13 @@
+# # import config file        place inside ~/.profile
+# . ~/.profile_usr.sh   # OR source ~/file/path
+
+# Email Config
+export MAIL_USERNAME=""
+export MAIL_PASSWORD=""
+
+
+# DataBase Config
+export DB_USER=""
+export DB_PASSWORD=""
+export DB_SERVER_IP=""      # localhost:3306
+export DB_NAME=""       # dev
